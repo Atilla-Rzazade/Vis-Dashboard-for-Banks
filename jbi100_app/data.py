4d01f444f7ae77c1dcc551b1fa78070478b8c90b
teyim <i.e.teymurlu@gmail.com>
2024-01-17 17:48:00 +0100

    Data cleaning is done

--- jbi100_app/data.py
+++ jbi100_app/data.py
@@ -1,10 +1,81 @@
 import plotly.express as px
-
+import pandas as pd
+import numpy as np
 
 def get_data():
     # Read data
-    df = px.data.iris()
+    df = pd.read_csv('Dataset/all_data.csv', sep=';', low_memory=False)
 
-    # Any further data preprocessing can go her
+    selected_columns = [
+        "Customer_ID", # tick
+        "Month", # tick
+        "Age", # tick
+        "Occupation", # tick
+        "Annual_Income", 
+        "Num_Bank_Accounts",# tick
+        "Num_of_Loan", # tick
+        "Type_of_Loan", # tick
+        "Num_of_Delayed_Payment", # tick
+        "Outstanding_Debt", # tick
+        "Credit_Utilization_Ratio", # tick
+    ]
 
+    df_selected = df[selected_columns]
+
+    empty_row_representation = ['_______', '_', '!@9#%8', '#F%$D@*&8', '__10000__']
+
+    numerical_columns = ['Age', 'Annual_Income', 'Num_Bank_Accounts', 'Num_of_Loan', 'Num_of_Delayed_Payment', 'Outstanding_Debt', 'Credit_Utilization_Ratio']
+
+    for column in numerical_columns:
+        df_selected.loc[:, column] = df_selected[column].apply(lambda x: str(x).replace('_', ''))
+        df_selected.loc[:, column] = df_selected[column].apply(lambda x: str(x).replace(',', '.'))
+
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+
+    df_selected.replace(empty_row_representation, np.nan, inplace=True)
+
+    df_selected = clean_data(df_selected, 'Age', 18, 100)
+    df_selected = clean_data(df_selected, 'Num_of_Loan', 0, 9)
+    df_selected = clean_data(df_selected, 'Num_Bank_Accounts', 0, 11)
+    df_selected = clean_data(df_selected, 'Num_of_Delayed_Payment', 0, 28)
+
+    refill_data(df_selected)
+    return df_selected
+
+
+def clean_data(df, column_name: str, lower_bound: float, upper_bound: float):
+    df[column_name] = pd.to_numeric(df[column_name], errors='coerce')
+    df[column_name] = np.where((df[column_name] >= upper_bound) | (df[column_name] < lower_bound), np.nan, df[column_name])
     return df
+
+def refill_data(df_selected):
+    columns_to_fill = ['Age', 'Occupation']
+
+    # Fill missing values
+    for column in columns_to_fill:
+        # Update the dictionary with the current state of the DataFrame
+        df_selected[column] = df_selected.groupby('Customer_ID')[column].apply(lambda group: group.fillna(method='ffill'))
+        df_selected[column] = df_selected.groupby('Customer_ID')[column].apply(lambda group: group.fillna(method='bfill'))
+
+    df_selected['Num_of_Delayed_Payment'] = df_selected.groupby('Customer_ID')['Num_of_Delayed_Payment'].transform(lambda x: x.fillna(round(x.median(), 1)))
+
+    def fill_with_mode(x):
+        return x.fillna(x.mode()[0]) if not x.mode().empty else x
+
+    def replace_with_mode(x):
+        return x.mode()[0] if not x.mode().empty else x
+
+    df_selected['Num_of_Loan'] = df_selected.groupby('Customer_ID')['Num_of_Loan'].transform(lambda x: x.fillna(fill_with_mode(x)))
+    df_selected['Num_Bank_Accounts'] = df_selected.groupby('Customer_ID')['Num_Bank_Accounts'].transform(lambda x: x.fillna(round(x.median(), 1)))
+    df_selected['Annual_Income'] = df_selected.groupby('Customer_ID')['Annual_Income'].transform(replace_with_mode)
+
+    def fill_num_of_loan(row):
+        if pd.isnull(row['Num_of_Loan']):
+            count = row['Type_of_Loan'].count(',')
+            return count + 1 if count > 0 else 0
+        else:
+            return row['Num_of_Loan']
+
+    df_selected['Num_of_Loan'] = df_selected.apply(fill_num_of_loan, axis=1)
+
+    return df_selected
